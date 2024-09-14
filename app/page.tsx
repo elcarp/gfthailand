fe751bf4c8257eb5748025ddebd088d01e7db7ec
Lise Carpenter <lise.carpenter@gmail.com>
2024-09-14 22:13:22 +0700

    use google map wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -2,73 +2,35 @@
 
 import Image from 'next/image'
 
-// import { Map, Marker } from 'pigeon-maps'
-import { useState } from 'react'
 import glutenBg from '~public/images/gluten-bg.jpg'
 
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 import React from 'react'
+// import { Suspense } from 'react';
+import { GoogleMap, GoogleMapApiLoader, Marker } from 'react-google-map-wrapper'
+// import { Fallback } from '../components';
 
-const containerStyle = {
-  width: '400px',
-  height: '400px',
+function Map() {
+  return (
+    // you can pass props to map container element.
+    // use Tailwind CSS or styled-components or anything to style your container.
+    <GoogleMap
+      className='h-full'
+      zoom={17}
+      center={{ lat: 37.5709413, lng: 126.977787 }}>
+      <Marker lat={37.5709413} lng={126.977787} />
+    </GoogleMap>
+  )
 }
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-}
-
 export default function Home() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'YOUR_API_KEY',
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  // const [hue, setHue] = useState(0)
-  // const color = `hsl(${hue % 360}deg 39% 70%)`
   return (
     <>
       <section className='flex'>
         <div className='h-screen w-1/2'>
-          isLoaded ? (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}>
-            {/* Child components, such as markers, info windows, etc. */}
-            <></>
-          </GoogleMap>
-          ) : <></>
-          {/* <Map defaultCenter={[13.743544035029018, 100.56214878374486]} defaultZoom={11}>
-            <Marker
-              width={50}
-              anchor={[50.879, 4.6997]}
-              color={color}
-              onClick={() => setHue(hue + 20)}
-            />
-            <Marker
-              width={50}
-              anchor={[50.879, 4.6997]}
-              color={color}
-              onClick={() => setHue(hue + 20)}></Marker>
-          </Map> */}
+          <GoogleMapApiLoader
+            apiKey='AIzaSyAc4TUtJCbR_8sdVpgAcJLKrEBKRVmMhVE'
+            suspense>
+            <Map />
+          </GoogleMapApiLoader>
         </div>
         <div className='w-1/2 relative h-screen'>
           <Image src={glutenBg} alt='gluten' layout='fill' objectFit='cover' />
