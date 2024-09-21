82f4061259bb468a4d0c2104ad286925d77a52a3
Lise Carpenter <lise.carpenter@gmail.com>
2024-09-21 12:10:36 +0700

    add dummy select list

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,48 @@ import Header from '~components/header'
 import Image from 'next/image'
 import Meal from '~public/images/gf-meal.jpg'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { Select } from '@headlessui/react'
 
 export default function Home() {
+  const neighborhoodOptions = [
+    {
+      label: 'Sukhumvit',
+      value: 'sukhumvit',
+    },
+    {
+      label: 'Silom',
+      value: 'silom',
+    },
+    {
+      label: 'Sathorn',
+      value: 'sathorn',
+    },
+    {
+      label: 'Bang Rak',
+      value: 'bang rak',
+    },
+    {
+      label: 'Chatuchak',
+      value: 'chatuchak',
+    },
+    {
+      label: 'Thonburi',
+      value: 'thonburi',
+    },
+    {
+      label: 'Bang Kapi',
+      value: 'bang kapi',
+    },
+    {
+      label: 'Phra Khanong',
+      value: 'phra kanong',
+    },
+    {
+      label: 'Ari',
+      value: 'ari',
+    },
+  ]
+
   return (
     <>
       <Header />
@@ -19,8 +59,17 @@ export default function Home() {
           <h1 className='text-2xl font-bold'>
             Explore the best gluten free restaurants in Thailand
           </h1>
-          <div className='rounded-xl bg-white w-full shadow-xl p-10'>
+          <div className='rounded-xl bg-white w-full flex shadow-xl p-10'>
             <MagnifyingGlassIcon aria-hidden='true' className='h-6 w-6' />
+            <Select className='focus:outline-none min-w-20 ml-4'>
+              {neighborhoodOptions.map(({ label, value }) => {
+                return (
+                  <>
+                    <option key={value} value={value}>{label}</option>
+                  </>
+                )
+              })}
+            </Select>
           </div>
         </div>
       </section>
