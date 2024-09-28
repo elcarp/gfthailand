667f27d2d3558b4deac9e44179dd444c9c83d9eb
Lise Carpenter <lise.carpenter@gmail.com>
2024-09-28 13:18:52 +0700

    replace logo and link to home

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -9,7 +9,11 @@ import Image from 'next/image'
 const navigation = [
   { name: 'Search', href: '#' },
   { name: 'Discover', href: '#' },
-  { name: 'Hospitality', href: 'http://www.glutenfreehospitality.com', target: '_blank' },
+  {
+    name: 'Hospitality',
+    href: 'http://www.glutenfreehospitality.com',
+    target: '_blank',
+  },
   { name: 'Blog', href: '#' },
 ]
 
@@ -22,8 +26,7 @@ export default function Example() {
         aria-label='Global'
         className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
         <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
-            <span className='sr-only'>Your Company</span>
+          <a href='/' className='-m-1.5 p-1.5'>
             <Image alt='' src={logo} className='h-16 w-auto' />
           </a>
         </div>
@@ -60,13 +63,8 @@ export default function Example() {
         <div className='fixed inset-0 z-10' />
         <DialogPanel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
-              <img
-                alt=''
-                src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-                className='h-8 w-auto'
-              />
+            <a href='/' className='-m-1.5 p-1.5'>
+              <Image alt='' src={logo} className='h-16 w-auto' />
             </a>
             <button
               type='button'
