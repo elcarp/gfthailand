fd5cf3cc72a27c37f8d5959efebd9d4f4e84c86e
Lise Carpenter <lise.carpenter@gmail.com>
2024-10-01 21:41:30 +0700

    WIP add restaurant form

---
+++ app/api/add-restaurant/route.ts
@@ -0,0 +1,31 @@
+import { kv } from '@vercel/kv'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  // const body = request.body
+
+  const body = {
+    name: 'Veganerie Silom',
+    neighborhood: 'Silom',
+    address: 'เลขที่ 651,653,655,657 661 บ้านสีลม ห้องเลขที่ A26 ชั้นที่ 01 663 Si Lom Rd, Silom, Bang Rak, Bangkok 10500',
+    coordinates: {
+      latitude: 13.724050793606516, 
+      longitude: 100.520360334908,
+    },
+    tags: ['vegan', 'plant-based', 'western', 'Thai'],
+  }
+
+  try {
+    await kv.hset('restaurant:veganerie-silom', body as any)
+    return NextResponse.json({
+      success: true,
+      message: 'Restaurant data added successfully',
+    })
+  } catch (error) {
+    console.error('Error adding restaurant data:', error)
+    return NextResponse.json(
+      { success: false, message: 'Failed to add restaurant data' },
+      { status: 500 }
+    )
+  }
+}
--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -5,6 +5,7 @@ import Header from '~components/header'
 
 export default function Manager(): any {
   const [restaurants, setRestaurants] = useState<any>()
+  const [restaurantName, setRestaurantName] = useState<string>()
 
   useEffect(() => {
     async function fetchRestaurants() {
@@ -18,12 +19,40 @@ export default function Manager(): any {
     fetchRestaurants()
   }, [])
   console.log(restaurants)
+
   const restaurantsList = restaurants && restaurants.data
 
+  const id = restaurantName && restaurantName.replace(/ /g, '-').toLowerCase()
+  
+
+  const handleSubmit = async () => {
+    // event.preventDefault()
+    await fetch('/api/add-restaurant', {
+      // body: JSONdata,
+
+      method: 'POST',
+    })
+    console.log('submitted!')
+  }
   return (
     <>
       <Header />
       <section className='py-20'>
+        <div className='max-w-4xl mx-auto py-10'>
+        <div>
+          <label htmlFor='email' className='sr-only'>
+            Email
+          </label>
+          <input
+            id='restaurantName'
+            name='Restaraunt Name'
+            type='text'
+            placeholder='Restaurant Name'
+            onChange={({ target }) => setRestaurantName(target.value)}
+            className='block w-1/4 px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          />
+        </div>
+        </div>
         <div className='px-4 sm:px-6 lg:px-8 container mx-auto'>
           <div className='sm:flex sm:items-center'>
             <div className='sm:flex-auto'>
@@ -37,6 +66,7 @@ export default function Manager(): any {
             <div className='mt-4 sm:ml-16 sm:mt-0 sm:flex-none'>
               <button
                 type='button'
+                onClick={() => handleSubmit()}
                 className='block rounded-md bg-pomelo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-pomelo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pomelo-600'>
                 Add restaurant
               </button>
