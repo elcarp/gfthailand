662274faed4199c09356ef47a5d774f9bd585c2c
Lise Carpenter <lise.carpenter@gmail.com>
2024-10-02 14:25:20 +0700

    handle delete restaurants modal

--- app/manager/protected-content.tsx
+++ app/manager/protected-content.tsx
@@ -12,6 +12,7 @@ export default function ProtectedContent(): any {
   const [longitude, setLongitude] = useState<number>()
   const [tags, setTags] = useState<any>()
   const [deleteId, setDeleteId] = useState<any>()
+  const [showDeleteModal, setShowDeleteModal] = useState<any>()
 
   const neighborhoodOptions = [
     {
@@ -83,6 +84,7 @@ export default function ProtectedContent(): any {
     fetchRestaurants()
   }, [])
   console.log(restaurants, 'all restaurants')
+  console.log(deleteId, 'deleteid')
 
   const restaurantsList = restaurants && restaurants.restaurants
 
@@ -111,6 +113,10 @@ export default function ProtectedContent(): any {
     })
     console.log('submitted!')
   }
+  function handleDeleteModal(id: any) {
+    setShowDeleteModal(true)
+    setDeleteId(id)
+  }
 
   const handleDelete = async () => {
     try {
@@ -123,6 +129,12 @@ export default function ProtectedContent(): any {
 
       if (response.ok) {
         console.log('deleted!')
+        setShowDeleteModal(false)
+        const response = await fetch('/api/get-restaurant')
+        if (response.ok) {
+          const data = await response.json()
+          setRestaurants(data)
+        }
       }
     } catch (error) {
       console.log('Failed to delete restaurant')
@@ -130,6 +142,28 @@ export default function ProtectedContent(): any {
   }
   return (
     <>
+      <div
+        className={`${
+          showDeleteModal == true ? 'block' : 'hidden'
+        } bg-white absolute rounded-xl p-10 top-1/2 left-0 right-0 mx-auto w-1/3 z-50 shadow-xl`}>
+        <span className='block text-center'>Are you sure?</span>
+        <div className='flex mt-5 justify-center w-full'>
+          {' '}
+          <button
+            type='button'
+            onClick={() => setShowDeleteModal(false)}
+            className='mx-2 block rounded-md transparent px-3 py-2 text-center text-sm font-semibold text-pomelo-600 hover:text-white shadow-sm hover:bg-pomelo-600 border-pomelo-600 border focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pomelo-600'>
+            Cancel
+          </button>
+          <button
+            type='button'
+            onClick={() => handleDelete()}
+            className='mx-2 block rounded-md bg-pomelo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-pomelo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pomelo-600'>
+            Yes, Delete
+          </button>
+        </div>
+      </div>
+
       <Header />
       <section className='py-20'>
         <div className='max-w-4xl mx-auto py-10 grid grid-cols-2 gap-4'>
@@ -246,7 +280,7 @@ export default function ProtectedContent(): any {
                   <tbody className='divide-y divide-gray-200'>
                     {restaurantsList &&
                       restaurantsList.map(
-                        ({ name, neighborhood, tags }: any) => (
+                        ({ name, neighborhood, tags, id }: any) => (
                           <tr key={name}>
                             <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0'>
                               {name}
@@ -255,22 +289,29 @@ export default function ProtectedContent(): any {
                               {neighborhood}
                             </td>
                             <td className='whitespace-nowrap px-3 py-4 text-sm text-gray-500'>
-                              {tags && tags.map((tag: string) => {
-                                return (
-                                  <>
-                                    <span className='capitalize mx-2 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10'>
-                                      {tag}
-                                    </span>
-                                  </>
-                                )
-                              })}
+                              {tags &&
+                                tags.map((tag: string) => {
+                                  return (
+                                    <>
+                                      <span className='capitalize mx-2 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10'>
+                                        {tag}
+                                      </span>
+                                    </>
+                                  )
+                                })}
                             </td>
 
                             <td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0'>
                               <a
                                 href='#'
-                                className='text-pomelo-600 hover:text-pomelo-900'>
+                                className='text-pomelo-600 hover:text-pomelo-900 mx-1'>
                                 Edit
+                                <span className='sr-only'>, {name}</span>
+                              </a>
+                              <a
+                                onClick={() => handleDeleteModal(id)}
+                                className='text-pomelo-600 hover:text-pomelo-900 mx-1 cursor-pointer'>
+                                Delete
                                 <span className='sr-only'>, {name}</span>
                               </a>
                             </td>
@@ -279,23 +320,6 @@ export default function ProtectedContent(): any {
                       )}
                   </tbody>
                 </table>
-              </div>
-              <div className='max-w-4xl mx-auto grid grid-cols-2 mt-20 gap-4'>
-                <input
-                  value={deleteId}
-                  className='block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                  placeholder='delete'
-                  onChange={({ target }) => setDeleteId(target.value)}
-                />
-
-                <div>
-                  <button
-                    type='button'
-                    onClick={() => handleDelete()}
-                    className='rounded-md bg-pomelo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-pomelo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pomelo-600'>
-                    Delete restaurant
-                  </button>
-                </div>
               </div>
             </div>
           </div>
