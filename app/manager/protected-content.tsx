5bd7f646882eab8f4bdc1a733ec09cc106dc6303
Lise Carpenter <lise.carpenter@gmail.com>
2024-10-03 09:14:06 +0700

    clean up

--- app/manager/protected-content.tsx
+++ app/manager/protected-content.tsx
@@ -95,8 +95,6 @@ export default function ProtectedContent(): any {
   useEffect(() => {
     fetchRestaurants()
   }, [])
-  console.log(restaurants, 'all restaurants')
-  console.log(deleteId, 'deleteid')
 
   const restaurantsList = restaurants && restaurants.restaurants
 
