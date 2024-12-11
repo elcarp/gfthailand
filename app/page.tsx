a0ed5dbd3bfd304e65867d7cd9e6b3f98ee83353
elcarp <lise.carpenter@gmail.com>
2024-12-11 14:26:07 +0700

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import dynamic from 'next/dynamic'
 import Header from '~/components/header'
 import { cuisines } from '~/constants'
 import { SingleValue } from 'react-select'
-import { PlacesAutocomplete } from '~components/places-autocomplete'
 
 const Select = dynamic(() => import('react-select'), { ssr: false })
 const GoogleMaps = dynamic(() => import('~/components/maps'), { ssr: false })
@@ -123,8 +122,7 @@ export default function Home() {
 
       <section>
         <div className='max-w-4xl py-20 mx-auto p-4'>
-          <PlacesAutocomplete />
-          {/* <GoogleMaps
+          <GoogleMaps
             apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
             center={{ lat: 13.7274902015168, lng: 100.57534908460062 }}
             zoom={14}
@@ -140,7 +138,7 @@ export default function Home() {
                 content: '<h3>Vistro</h3>',
               },
             ]}
-          /> */}
+          />
         </div>
       </section>
 
