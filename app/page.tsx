aef5f458678b0c26270cf08e4485bcb5fb01bd0b
elcarp <lise.carpenter@gmail.com>
2024-12-12 12:29:57 +0700

    link to restaurants

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import dynamic from 'next/dynamic'
 import Header from '~/components/header'
 import { cuisines } from '~/constants'
 import { SingleValue } from 'react-select'
+import Link from 'next/link'
 
 const Select = dynamic(() => import('react-select'), { ssr: false })
 const GoogleMaps = dynamic(() => import('~/components/maps'), { ssr: false })
@@ -42,6 +43,7 @@ const cuisineOptions: Option[] = cuisines.map((cuisine) => ({
 interface Restaurant {
   name: string
   photo: string
+  id: string
 }
 
 export default function Home() {
@@ -145,31 +147,33 @@ export default function Home() {
       <section className='bg-white max-w-4xl mx-auto rounded-xl shadow-xl p-10'>
         <h2 className={`${permanentMarker.className} text-4xl mb-6`}>Browse</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-          {restaurants.map(({ name, photo }) => (
-            <article
-              key={name}
-              className='relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80'>
-              <img
-                alt={name}
-                src={photo || '/images/default-food.jpg'}
-                className='absolute inset-0 -z-10 h-full w-full object-cover'
-              />
-              <div className='absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40' />
-              <div className='absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10' />
+          {restaurants.map(({ name, photo, id }) => (
+            <Link href={`/restaurants/${id}`} key={id}>
+              <article
+                key={name}
+                className='relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80'>
+                <img
+                  alt={name}
+                  src={photo || '/images/default-food.jpg'}
+                  className='absolute inset-0 -z-10 h-full w-full object-cover'
+                />
+                <div className='absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40' />
+                <div className='absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10' />
 
-              <div className='flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300'>
-                <div className='-ml-4 flex items-center gap-x-4'>
-                  <svg
-                    viewBox='0 0 2 2'
-                    className='-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50'>
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
+                <div className='flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300'>
+                  <div className='-ml-4 flex items-center gap-x-4'>
+                    <svg
+                      viewBox='0 0 2 2'
+                      className='-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50'>
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                  </div>
                 </div>
-              </div>
-              <h3 className='mt-3 text-lg leading-6 font-semibold text-white'>
-                {name}
-              </h3>
-            </article>
+                <h3 className='mt-3 text-lg leading-6 font-semibold text-white'>
+                  {name}
+                </h3>
+              </article>
+            </Link>
           ))}
         </div>
       </section>
